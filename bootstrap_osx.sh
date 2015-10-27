ed5b2e0ee31fe09da66113522e3029d9d0997e65
Nick Ficano <nficano@gmail.com>
2015-10-27 10:03:49 -0400

    added wget

--- bootstrap_osx.sh
+++ bootstrap_osx.sh
@@ -107,6 +107,7 @@ brew_install_or_upgrade 'redis'
 brew_install_or_upgrade 'shellcheck'
 brew_install_or_upgrade 'the_silver_searcher'
 brew_install_or_upgrade 'tmux'
+brew_install_or_upgrade 'wget'
 
 brew_install_or_upgrade 'bash'
 brew unlink bash
