efd593b28c042d1deac8e7020b0858662a85842b
Nick Ficano <nficano@gmail.com>
2017-12-11 09:56:39 -0500

    renamed for consistancy

--- dropbox-sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# -auto     accept all non-conflicting actions
-# -batch    ask no questions
-# -prefer   if conflict, which directory takes precedence
-# -silent   stfu
-
-unison $HOME/Library/Fonts/ $HOME/Dropbox/Fonts/ -auto -batch -prefer $HOME/Library/Fonts/
